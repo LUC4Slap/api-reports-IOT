02974b2728a646155136f18c74107c00afcd6a5a
lucas__almeida <lucaslap27@gmail.com>
2023-10-30 13:51:40 -0400

    feat: ajusta conexão com banco atlas e faz query para trazer geristros

--- conn.py
+++ conn.py
@@ -5,24 +5,25 @@ load_dotenv()
 
 class GetReposts:
     def __init__(self):
-        # mongodb+srv://lucasalmeida:lucasalmeida12@radar-pt.ial6lyr.mongodb.net/?retryWrites=true&w=majority
-        # mongodb+srv://lucasalmeida:lucasalmeida12@radar-pt.3rljncc.mongodb.net/
-        self.cliente = MongoClient("mongodb://localhost:27017/radar-pt") # PARA RODAR NO LOCALHOST
+        # self.cliente = MongoClient("mongodb://localhost:27017/radar-pt") # PARA RODAR NO LOCALHOST
         self.password = os.getenv("PASSWORD_DB")
         # SE FOR RODAR NO LOCAL COMENTAR A LINHA DE BAIXO
-        # self.cliente = MongoClient(f"mongodb+srv://lucasalmeida:{self.password}@radar-pt.3rljncc.mongodb.net/")
+        self.cliente = MongoClient(f"mongodb+srv://lucasalmeida:{self.password}@radar-pt.3rljncc.mongodb.net/")
         self.db = self.cliente["radar-pt"]
         self.collection = self.db.reports
         self.results = None
+        self.results_array = []
 
     def getReportDB(self):
         try:
-            self.results = self.collection.find()
+            self.results = self.collection.find({}, {'_id': False})
         except Exception as error:
             print("erro para inserir")
             print(error)
 
     def returnDB(self):
       self.getReportDB()
-      print(self.results)
-      return self.results
+      for doc in self.results:
+          self.results_array.append(doc)
+      print(self.results_array)
+      return self.results_array
