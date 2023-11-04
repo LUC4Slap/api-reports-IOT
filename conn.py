1ff8f942cbff46664ec5101c5574a81d90932629
lucas__almeida <lucaslap27@gmail.com>
2023-11-03 20:26:13 -0400

    feat: cria query de busca de repot mais recente

--- conn.py
+++ conn.py
@@ -5,18 +5,40 @@ load_dotenv()
 
 class GetReposts:
     def __init__(self):
-        # self.cliente = MongoClient("mongodb://localhost:27017/radar-pt") # PARA RODAR NO LOCALHOST
-        self.password = os.getenv("PASSWORD_DB")
+        self.cliente = MongoClient("mongodb://localhost:27017/radar-pt") # PARA RODAR NO LOCALHOST
+        # self.password = os.getenv("PASSWORD_DB")
         # SE FOR RODAR NO LOCAL COMENTAR A LINHA DE BAIXO
-        self.cliente = MongoClient(f"mongodb+srv://lucasalmeida:{self.password}@radar-pt.3rljncc.mongodb.net/")
+        # self.cliente = MongoClient(f"mongodb+srv://lucasalmeida:{self.password}@radar-pt.3rljncc.mongodb.net/")
         self.db = self.cliente["radar-pt"]
         self.collection = self.db.reports
         self.results = None
         self.results_array = []
 
     def getReportDB(self):
+        # query
+        pipeline = [
+            {
+                "$sort": {
+                    "date_report": -1
+                }
+            },
+            {
+                "$group": {
+                    "_id": "$imei",
+                    "latest_record": {
+                        "$first": "$$ROOT"
+                    }
+                }
+            },
+            {
+                "$replaceRoot": {
+                    "newRoot": "$latest_record"
+                }
+            }
+        ]
+
         try:
-            self.results = self.collection.find({}, {'_id': False})
+            self.results = list(self.collection.aggregate(pipeline))
         except Exception as error:
             print("erro para inserir")
             print(error)
@@ -26,5 +48,4 @@ class GetReposts:
       self.results_array = []
       for doc in self.results:
           self.results_array.append(doc)
-      print(self.results_array)
       return self.results_array
